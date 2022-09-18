4f9ea407454274b67052b397db65cd5829369e5a
reinahandal <handalreina@gmail.com>
2022-09-18 13:51:01 +0300

    working solution including the route that changes colors

--- flask/flask_fundamentals/checkerboard/server.py
+++ flask/flask_fundamentals/checkerboard/server.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def eight():
-    return render_template("index.html", rows=8, cols=8)
+    return render_template("index.html", rows=8, cols=8, color1="black", color2="red")
 @app.route('/4')
 def four():
-    return render_template("index.html", rows=4, cols=8)
+    return render_template("index.html", rows=4, cols=8, color1="black", color2="red")
 @app.route('/<x>/<y>')
 def custom(x,y):
-    return render_template("index.html", rows=int(x), cols=int(y))
-
+    return render_template("index.html", rows=int(x), cols=int(y), color1="black", color2="red")
+@app.route('/<x>/<y>/<color1>/<color2>')
+def customcolors(x,y,color1,color2):
+    return render_template("index.html", rows=int(x), cols=int(y), color1=color1, color2=color2)
 if __name__ == "__main__":
     app.run(debug=True)

4e31b68b57ef2918e900208ed29098c6bd004d01
reinahandal <handalreina@gmail.com>
2022-09-21 19:46:16 +0300

    removed unnecessary lines from views.py

--- django/django_intro/counter/counter_app/views.py
+++ django/django_intro/counter/counter_app/views.py
@@ -3,19 +3,13 @@ from django.shortcuts import render, redirect
 def index(request):
     counter = request.session.get('counter', 0)
     request.session['counter'] = counter + 1
-    print('this is counter 1: ' + str(counter))
     return render(request, 'index.html')
 def destroy(request):
     del request.session['counter']
-    # del request.session['temp']
     return redirect('/')
 def add_two(request):
     counter2 = request.session.get('counter', 0)
     request.session['counter'] = counter2 + 1
-    print('this is counter 2: ' + str(counter2)) 
-    # temp = request.session.get('temp', 2)
-    # request.session['temp'] = temp + 2
-    # print('this is temp: ' + str(temp)) 
     return redirect('/')
 def custom_number(request):
     counter3 = request.session.get('counter', 0)

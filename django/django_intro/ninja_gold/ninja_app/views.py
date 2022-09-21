c0e32e3ae9eec7bd2f64d5d57699a50f771e021c
reinahandal <handalreina@gmail.com>
2022-09-22 00:00:34 +0300

    activity log - only updates current p, does not add a new one

--- django/django_intro/ninja_gold/ninja_app/views.py
+++ django/django_intro/ninja_gold/ninja_app/views.py
@@ -7,11 +7,14 @@ def money(request):
     if request.POST['activity'] == 'farm' or request.POST['activity'] == 'cave' or request.POST['activity'] == 'house':
         random_number = random.randint(10, 20)
         request.session['random_number'] = random_number
+        request.session['current_activity'] = request.POST['activity']
     elif request.POST['activity'] == 'quest':
         random_number = random.randint(-50, 50)
         request.session['random_number'] = random_number
+        request.session['current_activity'] = request.POST['activity']
     request.session['gold_counter'] += random_number
     return redirect('/gold/')
 def reset(request):
     request.session['gold_counter'] = 0
+    del request.session['current_activity']
     return redirect('gold/')

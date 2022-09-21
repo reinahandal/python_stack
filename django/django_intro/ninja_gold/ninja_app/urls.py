65005b541a03d92ce96b95a9c717644b745220f1
reinahandal <handalreina@gmail.com>
2022-09-21 23:15:53 +0300

    added route and method that clears gold counter upon opening the page

--- django/django_intro/ninja_gold/ninja_app/urls.py
+++ django/django_intro/ninja_gold/ninja_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('', views.index),
-    path('process_money', views.money)
+    path('', views.reset),
+    path('gold/', views.index),
+    path('process_money/', views.money)
 ]
--- django/django_intro/ninja_gold/ninja_app/views.py
+++ django/django_intro/ninja_gold/ninja_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 import random
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request,'index.html')
 def money(request):
     if request.POST['activity'] == 'farm' or request.POST['activity'] == 'cave' or request.POST['activity'] == 'house':
         random_number = random.randint(10, 20)
@@ -10,6 +10,8 @@ def money(request):
     elif request.POST['activity'] == 'quest':
         random_number = random.randint(-50, 50)
         request.session['random_number'] = random_number
-    gold_counter = request.session.get('gold_counter', 0)
-    request.session['gold_counter'] = gold_counter + random_number
-    return redirect('/')
+    request.session['gold_counter'] += random_number
+    return redirect('/gold/')
+def reset(request):
+    request.session['gold_counter'] = 0
+    return redirect('gold/')

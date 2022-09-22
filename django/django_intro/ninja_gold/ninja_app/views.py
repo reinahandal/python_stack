d882b0ad51e46f17d13762c6f62fcdc09c3e3636
reinahandal <handalreina@gmail.com>
2022-09-22 13:09:22 +0300

    added time to the activity log

--- django/django_intro/ninja_gold/ninja_app/views.py
+++ django/django_intro/ninja_gold/ninja_app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 import random
+from time import strftime, localtime
 
 def index(request):
     return render(request,'index.html')
 
 def money(request):
+    time = strftime("%b %d, %Y, %H:%M %p", localtime())
     if request.POST['activity'] == 'farm' or request.POST['activity'] == 'cave' or request.POST['activity'] == 'house':
         random_number = random.randint(10, 20)
         current_activity = request.POST['activity']
         request.session['random_number'] = random_number
         request.session['current_activity'] = current_activity
-        request.session['activity_log'].insert(0, ['You entered a '+ current_activity + ' and earned ' + str(random_number) + ' gold.', 'success'])
+        request.session['activity_log'].insert(0, ['You entered a '+ current_activity + ' and earned ' + str(random_number) + ' gold. (' + time + ')', 'success'])
 
     elif request.POST['activity'] == 'quest':
         random_number = random.randint(-50, 50)
@@ -18,9 +20,9 @@ def money(request):
         request.session['random_number'] = random_number
         request.session['current_activity'] = current_activity
         if random_number < 0:
-            request.session['activity_log'].insert(0, ['You failed a quest and lost ' + str(random_number) + ' gold. Ouch.', 'lose'])
+            request.session['activity_log'].insert(0, ['You failed a quest and lost ' + str(random_number) + ' gold. Ouch. (' + time + ')', 'lose'])
         else:
-            request.session['activity_log'].insert(0, ['You entered a quest and earned ' + str(random_number) + ' gold.', 'success'])
+            request.session['activity_log'].insert(0, ['You entered a quest and earned ' + str(random_number) + ' gold. (' + time + ')', 'success'])
     request.session['gold_counter'] += random_number
     return redirect('/gold/')
 

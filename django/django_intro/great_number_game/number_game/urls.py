51fa89ae9c40a0b8a0e151404e5b44620810767d
reinahandal <handalreina@gmail.com>
2022-09-22 13:38:44 +0300

    removed the 5 tries limit because its not working

--- django/django_intro/great_number_game/number_game/urls.py
+++ django/django_intro/great_number_game/number_game/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index),
     path('submit_guess/', views.submit_guess),
     path('play_again/', views.play_again),
-    path('you_lose/', views.you_lose),
+    # path('you_lose/', views.you_lose),
 ]
--- django/django_intro/great_number_game/number_game/views.py
+++ django/django_intro/great_number_game/number_game/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, HttpResponse
+
+
 def index(request):
     import random
     if "random_number" not in request.session:
         random_number = random.randint(1, 100)
         request.session['random_number'] = random_number
     return render(request,'index.html')
+
+
 def submit_guess(request):
     counter = request.session.get('guess_counter', 1)
     request.session['guess_counter'] = counter + 1
@@ -16,13 +20,17 @@ def submit_guess(request):
     elif random_guess == request.session['random_number']:
         request.session['user_guess'] = "correct"
     return redirect('/')
+
+
 def play_again(request):
     del request.session['random_number']
     del request.session['user_guess']
     del request.session['guess_counter']
     return redirect('/')
-def you_lose(request):
-    del request.session['random_number']
-    del request.session['user_guess']
-    del request.session['guess_counter']
-    return redirect('/')
+
+# def you_lose(request):
+#     del request.session['random_number']
+#     if 'user_guess' not in request.session:
+#         del request.session['user_guess']  
+#     del request.session['guess_counter']
+#     return redirect('/')

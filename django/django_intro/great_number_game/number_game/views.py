97d959118644384385fd23376d6128c8a5cde182
reinahandal <handalreina@gmail.com>
2022-09-25 13:26:21 +0300

    fixing counter

--- django/django_intro/great_number_game/number_game/views.py
+++ django/django_intro/great_number_game/number_game/views.py
@@ -6,11 +6,12 @@ def index(request):
     if "random_number" not in request.session:
         random_number = random.randint(1, 100)
         request.session['random_number'] = random_number
+        print(random_number)
     return render(request,'index.html')
 
 
 def submit_guess(request):
-    counter = request.session.get('guess_counter', 1)
+    counter = request.session.get('guess_counter', 0)
     request.session['guess_counter'] = counter + 1
     random_guess = int(request.POST['random_guess'])
     if random_guess > request.session['random_number']:
@@ -30,7 +31,6 @@ def play_again(request):
 
 # def you_lose(request):
 #     del request.session['random_number']
-#     if 'user_guess' not in request.session:
-#         del request.session['user_guess']  
+#     del request.session['user_guess']  
 #     del request.session['guess_counter']
 #     return redirect('/')

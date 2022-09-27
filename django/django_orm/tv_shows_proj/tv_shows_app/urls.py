fba8b67682224d4d126f50c4b4be203de34e5179
reinahandal <handalreina@gmail.com>
2022-09-27 23:11:53 +0300

    all 4 required templates with the corresponding routes, with working update form and a delete method

--- django/django_orm/tv_shows_proj/tv_shows_app/urls.py
+++ django/django_orm/tv_shows_proj/tv_shows_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('shows/', views.index),
     path('shows/new/', views.add_show),
     path('shows/create/', views.create_show),
+    path('shows/<show_id>/', views.read_show),
+    path('shows/<show_id>/edit/', views.edit_show),
+    path('shows/<show_id>/update/', views.update_show),
+    path('shows/<show_id>/destroy/', views.delete_show),
 ]
--- django/django_orm/tv_shows_proj/tv_shows_app/views.py
+++ django/django_orm/tv_shows_proj/tv_shows_app/views.py
@@ -16,6 +16,42 @@ def create_show(request):
     network=request.POST['network']
     release_date=request.POST['release_date']
     desc=request.POST['desc']
-    Show.objects.create(title=title, network=network, release_date=release_date, desc=desc)
-    return redirect('/shows')
+    new_show = Show.objects.create(title=title, network=network, release_date=release_date, desc=desc)
+    show_id = new_show.id
+    return redirect('/shows/'+str(show_id)+'/')
     # must redirect to /shows/<id>
+
+
+def read_show(request, show_id):
+    context = {
+        "tv_show": Show.objects.get(id=show_id),
+    }
+    return render(request, 'read.html', context)
+
+
+def edit_show(request, show_id):
+    context = {
+        "tv_show": Show.objects.get(id=show_id),
+    }
+    return render(request, 'update.html', context)
+
+
+def update_show(request, show_id):
+    new_title=request.POST['title']
+    new_network=request.POST['network']
+    new_date=request.POST['release_date']
+    new_desc=request.POST['desc']
+    show_to_update = Show.objects.get(id=show_id)
+    show_to_update.title = new_title
+    show_to_update.network = new_network
+    show_to_update.date = new_date
+    show_to_update.desc = new_desc
+    show_to_update.save()
+    return redirect('/shows/'+show_id)
+# updated_at datetime, can we update it ???
+
+
+def delete_show(request, show_id):
+    show_to_delete = Show.objects.get(id=show_id)
+    show_to_delete.delete()
+    return redirect('/shows')

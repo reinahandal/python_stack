84792bea9988e493d7f36b327cdfc4878b2319e1
reinahandal <handalreina@gmail.com>
2022-09-28 14:07:44 +0300

    minor changes to reduce code and remove minus sign from lost gold

--- django/django_intro/ninja_gold/ninja_app/views.py
+++ django/django_intro/ninja_gold/ninja_app/views.py
@@ -3,23 +3,24 @@ import random
 from time import strftime, localtime
 
 def index(request):
+    if 'gold_counter' and 'activity_log' not in request.session:
+        request.session['gold_counter'] = 0
+        request.session['activity_log'] = []
     return render(request,'index.html')
 
 def money(request):
     time = strftime("%b %d, %Y, %H:%M %p", localtime())
+    random_number = random.randint(-50, 50) if request.POST['activity'] == 'quest' else random.randint(10, 20)
     if request.POST['activity'] == 'farm' or request.POST['activity'] == 'cave' or request.POST['activity'] == 'house':
-        random_number = random.randint(10, 20)
         current_activity = request.POST['activity']
-        request.session['random_number'] = random_number
         request.session['current_activity'] = current_activity
         request.session['activity_log'].insert(0, ['You entered a '+ current_activity + ' and earned ' + str(random_number) + ' gold. (' + time + ')', 'success'])
 
     elif request.POST['activity'] == 'quest':
-        random_number = random.randint(-50, 50)
         current_activity = request.POST['activity']
-        request.session['random_number'] = random_number
         request.session['current_activity'] = current_activity
         if random_number < 0:
+            random_number = random_number * -1
             request.session['activity_log'].insert(0, ['You failed a quest and lost ' + str(random_number) + ' gold. Ouch. (' + time + ')', 'lose'])
         else:
             request.session['activity_log'].insert(0, ['You entered a quest and earned ' + str(random_number) + ' gold. (' + time + ')', 'success'])
@@ -28,8 +29,8 @@ def money(request):
     return redirect('/gold/')
 
 def reset(request):
-    request.session['gold_counter'] = 0
-    request.session['activity_log'] = []
     if "current_activity" in request.session:
         del request.session['current_activity']
+        del request.session['gold_counter']
+        del request.session['activity_log']
     return redirect('gold/')

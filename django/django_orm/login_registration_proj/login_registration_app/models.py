52013d94d7a7d6872933f6973f2fa79feda63c3e
reinahandal <handalreina@gmail.com>
2022-10-02 23:20:43 +0300

    moved some methods to models and cleaned up code

--- django/django_orm/login_registration_proj/login_registration_app/models.py
+++ django/django_orm/login_registration_proj/login_registration_app/models.py
@@ -1,23 +1,18 @@
 from django.db import models
 from datetime import datetime
 import re
+import bcrypt
 
 class UserManager(models.Manager):
-    def basic_validator(self,postData):
+    def reg_validator(self,postData):
         errors = {}
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         PASSWORD_REGEX = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,}$")
         NAME_REGEX = re.compile('^[A-Za-z]{2,}$')
 
-        # if len(postData['fname']) < 2 or not postData['fname'].isalpha():
-        #     errors["fname"] = "First name should contain letters only and be at least 2 characters" 
-
-        # if len(postData['lname']) < 2 or not postData['lname'].isalpha():
-        #     errors["lname"] = "Last name should contain letters only and be at least 2 characters"
-
-        if not NAME_REGEX.match(postData['fname']):
+        if not NAME_REGEX.match(postData['first_name']):
             errors["fname"] = "First name should contain letters only and be at least 2 characters"
-        if not NAME_REGEX.match(postData['lname']):
+        if not NAME_REGEX.match(postData['last_name']):
             errors["lname"] = "Last name should contain letters only and be at least 2 characters" 
         
         if not postData['email']:
@@ -30,13 +25,13 @@ class UserManager(models.Manager):
         if not postData['birthday']:
             errors['birthday'] = "Date of birth is required"
         elif datetime.strptime(postData['birthday'],'%Y-%m-%d') > datetime.today():
-                errors["birthday"] = "Date of birth should be in the past"
+            errors["birthday"] = "Date of birth should be in the past"
 
-        if not PASSWORD_REGEX.match(postData['pw']):
+        if not PASSWORD_REGEX.match(postData['password']):
             errors["password"] = "Password should be at least 8 characters, and contain at least one uppercase, one lowercase, one digit and one special character"
         if not postData['confirm_pw']:
             errors["confirm_password"] = "Please confirm password"
-        elif postData['pw'] != postData['confirm_pw']:
+        elif postData['password'] != postData['confirm_pw']:
             errors["password_match"] = "Passwords don't match"
         return errors
 
@@ -49,3 +44,19 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
+
+
+#This method creates the User object and stores the hashed password
+def registration(request):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email = request.POST['email']
+    birthday = request.POST['birthday']
+    password = request.POST['password']
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    User.objects.create(first_name=first_name, last_name=last_name, email=email, birthday=birthday, password=pw_hash)
+    request.session['logged_user'] = first_name
+
+# this method checks if password entered at login matches the hashed password in DB
+def is_authenticated(request, logged_user):
+    return bcrypt.checkpw(request.POST['login_pw'].encode(), logged_user.password.encode())
--- django/django_orm/login_registration_proj/login_registration_app/views.py
+++ django/django_orm/login_registration_proj/login_registration_app/views.py
@@ -1,52 +1,41 @@
 from django.shortcuts import render, redirect
 from .models import User
+from . import models
 from django.contrib import messages
-import bcrypt
+
 
 def index(request):
     return render(request, 'index.html')
 
+# This method executes the registration method if no errors were found in validation
 def registration(request):
-    errors = User.objects.basic_validator(request.POST)
+    errors = User.objects.reg_validator(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
             messages.error(request, value)
         return redirect('/')
     else:
-        first_name = request.POST['fname']
-        last_name = request.POST['lname']
-        email = request.POST['email']
-        birthday = request.POST['birthday']
-        password = request.POST['pw']
-        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode() # hashes the entered password
-        User.objects.create(first_name=first_name, last_name=last_name, email=email, birthday=birthday, password=pw_hash)
-        request.session['logged_user'] = first_name # creates session
+        models.registration(request)
         return redirect('/success')
 
-
+#This method checks if user email exists in DB (i.e if user is registered)
 def login(request):
-    # user = User.objects.filter(email=request.POST['login_email']) # checks if user email exists in DB, returns arr
-    # if user:
-    #     logged_user = user[0] # considering we have unique emails due to validation
-    #     if bcrypt.checkpw(request.POST['login_pw'].encode(), logged_user.password.encode()): # checks that login pw matches one in DB after hashing
-    #         request.session['logged_user'] = logged_user.first_name #creates session
-    #         return redirect('/success')
-    # return redirect('/') # if user doesnt exist, redirect to root route
     try:
         logged_user = User.objects.get(email=request.POST['login_email'])
     except:
         return redirect('/')
     else:
-        if bcrypt.checkpw(request.POST['login_pw'].encode(), logged_user.password.encode()):
+        if models.is_authenticated(request, logged_user):
             request.session['logged_user'] = logged_user.first_name
             return redirect('/success')
 
-
+#This method renders a page only for registered/logged in users, and doesnt allow access through a get request in the url
 def success(request):
-    if 'logged_user' in request.session: # doesnt allow user that isnt logged in to access this page 
+    if 'logged_user' in request.session:
         return render(request, 'success.html')
     else:
         return redirect('/')
+
 
 def logout(request):
     del request.session['logged_user']
